ee41e6053220955dc853e5f0032339b287aed50b
KKobuszewski <konrad.kobuszewski93@gmail.com>
2023-08-06 17:25:35 +0200

    Starting of CutOptimizer class writing

--- _optimcut.pyx
+++ _optimcut.pyx
@@ -14,14 +14,14 @@ from libc.stdio cimport printf
 #cdef extern from "<algorithm>" namespace "std":
 
 
-
+# https://cvanelteren.github.io/post/cython_templates/
 
 # ========================================================  FILE PROCESSING  ============================================================
 
 cdef extern from "optimcut.h" namespace "optimcut" nogil:
-    void initialize(int seed);
-    void finalize();
-    void _swap_order[T](T* state, int n);
+    void initialize(int seed)
+    void finalize()
+    void _swap_order[T](T* state, int n)
     void _cuts_to_material[T](T* state, int* material_id, T* material_length, int n)
     void _material_leftovers[T](T* state, int* material_id, T* material_length, T* leftovers, int n)
     #int ...
@@ -47,7 +47,7 @@ def cuts_to_material( np.ndarray[np.float64_t,ndim=1,negative_indices=False,mode
         initialize(0)
     
     cdef int  n     = <int>  state.size
-    cdef int* c_ptr = <int*> &material_id[0]; # need to convert pointer from np.int32_t explicitly?
+    cdef int* c_ptr = <int*> &material_id[0] # need to convert pointer from np.int32_t explicitly?
     _cuts_to_material[double](&state[0], c_ptr, &material_length[0], n)
 
     if (testing is True):
@@ -63,9 +63,42 @@ def material_leftovers( np.ndarray[np.float64_t,ndim=1,negative_indices=False,mo
         initialize(0)
     
     cdef int  n     = <int>  state.size
-    cdef int* c_ptr = <int*> &material_id[0];
+    cdef int* c_ptr = <int*> &material_id[0]
     _material_leftovers[double](&state[0],c_ptr, &material_length[0], &leftovers[0], n)
     
     if (testing is True):
         finalize()
 
+
+
+class CutOptimizer(object):
+    def __init__(self, name, operator):
+        self.name = name
+        self.operator = operator
+
+    def __call__(self, *operands):
+        return self.operator(*operands)
+
+
+cdef class cpCutOptimizer:
+
+    # Not available in Python-space:
+    cdef double offset
+
+    # Available in Python-space:
+    cdef public np.ndarray[np.float64_t,ndim=1,negative_indices=False,mode='c'] state
+    cdef public np.ndarray[np.int32_t,  ndim=1,negative_indices=False,mode='c'] material_id
+    cdef public np.ndarray[np.float64_t,ndim=1,negative_indices=False,mode='c'] material_length
+    cdef public np.ndarray[np.float64_t,ndim=1,negative_indices=False,mode='c'] leftovers
+
+    # Available in Python-space:
+    @property
+    def period(self):
+        return 1.0 / self.freq
+
+    @period.setter
+    def period(self, value):
+        self.freq = 1.0 / value
+    
+    def __cinit__(self): # cannot be overloaded
+        ...
--- optimcut.h
+++ optimcut.h
@@ -6,6 +6,7 @@
 #include <numeric>
 #include <gsl/gsl_qrng.h>
 #include <string.h>
+#include <float.h>
 
 
 /* ***************************************************************************************************************** * 
@@ -66,7 +67,7 @@ inline void _cuts_to_material(T* state, int* material_id, T* material_length, co
     T   current_length = 0.0;
     int current_material = 0;
     
-    for (int it; it<n; it++)
+    for (int it=0; it<n; it++)
     {
         current_length += state[it];
         
@@ -90,15 +91,15 @@ inline void _material_leftovers(T* state, int* material_id, T* material_length, 
     
     int current_id = 0;
     T sum = 0.0;
-    for (int i=0; i<n; i++)
+    for (int it=0; it<n; it++)
     {
-        if (current_id != material_id[i])
+        if (current_id != material_id[it])
         {
             leftovers[current_id] = material_length[current_id] - sum;
-            sum = 0.0;
+            sum         = 0.0;
             current_id += 1;
         }
-        sum += state[i];
+        sum += state[it];
     }
     
     
@@ -114,7 +115,8 @@ inline T _cost_function(T* state, int* material_id, T* material_length, T* lefto
 
 
 template <typename T>
-T _reconfigure(T* state, int* material_id, T* material_length, T* leftovers, const T old_costf, const int n)
+T _reconfigure(T* state, int* material_id, T* material_length, T* leftovers, 
+               const T old_costf, const T temp, const int n)
 {
     T ap    = 0.0;
     T costf = 0.0;
@@ -126,13 +128,72 @@ T _reconfigure(T* state, int* material_id, T* material_length, T* leftovers, con
         _cuts_to_material<T>(state, material_id, material_length, n);
         
         // compute cost function & acceptance probability
-        costf = _cost_function(state, material_id, material_length, leftovers, n);
-        
-    } while(ap < random<T>());
+        costf = _cost_function<T>(state, material_id, material_length, leftovers, n);
+        ap = std::exp( old_costf - costf ); // old_cost > costf  =>  ap > 1
+    } while (ap < random<T>());             // accept new config. if ap > random num. from [0,1]
     
     return costf; // we need to know current cost function value in the next step
 }
 
+template<typename T>
+void make_iterations(T* state, int* material_id, T* material_length, T* leftovers, 
+                     const int niter, const T temp, const int n)
+{
+    // get material ids & compute cost function
+    _cuts_to_material<T>(state, material_id, material_length, n);
+    T costf      = _cost_function(state, material_id, material_length, leftovers, n);
+    
+    // mem buffer to store best solution
+    T  best_costf = static_cast<T>( FLT_MAX ); // T could be possible float or double, so should be large enough
+    T* best_state = new T[n];
+
+    for (int it=0; it < niter; it++)
+    {
+        // const T old_costf = costf; // needed?
+        costf = _reconfigure<T>(state, material_id, material_length, leftovers, costf, temp, n);
+        
+        // if we have better solution we save it to mem buffer
+        if (best_costf > costf)
+        {
+            for (int jt=0; jt<n; jt++) { best_state[jt] = state[jt]; }
+            best_costf = costf;
+        }
+    }
+
+    // while number of required reconfigurations is done, fill state array with best solution and clear mem
+    for (int jt=0; jt<n; jt++) { state[jt] = best_state[jt]; }
+    delete best_state;
+}
+
+
+/*
+ *  T* states - array of length (niter+1)*n to collect states of 
+ *  T* costfs - array of length  niter+1 to collect values of cost function
+ *
+ *
+ */
+template<typename T>
+void make_iterations_with_save(T* states, int* material_id, T* material_length, T* leftovers, T* costfs,
+                               const int niter, const T temp, const int n)
+{
+    // get material ids & compute cost function
+    _cuts_to_material<T>(states, material_id, material_length, n);
+    costfs[0] = _cost_function(states, material_id, material_length, leftovers, n);
+    
+    // mem buffer to store costf from previous iteration
+    T  costf = static_cast<T>( FLT_MAX ); // T could be possible float or double, so should be large enough
+
+    for (int it=0; it < niter; it++)
+    {
+        // 
+        for (int jt=0; jt<n; jt++) { states[(it+1)*n+jt] = states[it*n+jt]; }
+
+        // const T old_costf = costf; // needed?
+        costf = _reconfigure<T>(states+(it+1)*n, material_id, material_length, leftovers, costf, temp, n);
+        
+        costfs[it+1] = costf;
+    }
+}
 
 
 
